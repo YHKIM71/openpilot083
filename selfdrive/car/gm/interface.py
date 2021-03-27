baef47eb437f3f205c38212e55e140adba173dc6
Linu Choi <linu1983@gmail.com>
2021-03-27 21:52:54 +0900

    fix

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -37,7 +37,7 @@ class CarInterface(CarInterfaceBase):
     # Start with a baseline lateral tuning for all GM vehicles. Override tuning as needed in each model section below.
     ret.minSteerSpeed = 7 * CV.MPH_TO_MS
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.00]]
+    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.13], [0.02]]
     ret.lateralTuning.pid.kf = 0.00004   # full torque for 20 deg at 80mph means 0.00007818594
     ret.steerRateCost = 0.7
     ret.steerActuatorDelay = 0.1  # Default delay, not measured yet
