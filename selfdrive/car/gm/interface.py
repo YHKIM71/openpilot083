a0f37f4a2c94e423475569c4f1e33ac718574fdd
linu1983 <linu1983@gmail.com>
2021-03-29 06:34:54 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -164,16 +164,16 @@ class CarInterface(CarInterfaceBase):
 
     events = self.create_common_events(ret, pcm_enable=False)
 
-#    if ret.vEgo < self.CP.minEnableSpeed:
-#      events.add(EventName.belowEngageSpeed)
+    if ret.vEgo < self.CP.minEnableSpeed:
+      events.add(EventName.belowEngageSpeed)
     if self.CS.park_brake:
       events.add(EventName.parkBrake)
     if ret.cruiseState.standstill:
       events.add(EventName.resumeRequired)
     if self.CS.pcm_acc_status == AccState.FAULTED:
       events.add(EventName.controlsFailed)
-    if ret.vEgo < self.CP.minSteerSpeed:
-      events.add(car.CarEvent.EventName.belowSteerSpeed)
+#    if ret.vEgo < self.CP.minSteerSpeed:
+#      events.add(car.CarEvent.EventName.belowSteerSpeed)
     if self.CS.autoHoldActivated:
       events.add(car.CarEvent.EventName.autoHoldActivated)
 	  
