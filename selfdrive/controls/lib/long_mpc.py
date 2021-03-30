09664cc79fe1df81e34a010de484ddb7542b81d6
linu1983 <linu1983@gmail.com>
2021-03-30 21:29:00 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -100,20 +100,16 @@ class LongitudinalMpc():
     # Calculate conditions
     self.v_rel = v_lead - v_ego   # calculate relative velocity vs lead car
 
-    # interpolation values
-#    v_ego_BP = [3., 40.]
-#    v_ego_PROFILE = [0.9, 1.3]
-#    v_rel_BP = [-0.1, 2.]
-#    v_rel_PROFILE = [0., 0.4]
-    
     # Calculate mpc
     t = sec_since_boot()
     cruise_gap = int(clip(CS.cruiseGap, 1., 3.))
-    TR = interp(float(cruise_gap), [1., 2., 3.], [0.9, 1.3, 1.8])
-#    TRatio = interp(float(cruise_gap), [1., 2., 3.], [1.0, 1.2, 1.5])	
-#    TR = interp(v_ego, v_ego_BP, v_ego_PROFILE) + interp(-self.v_rel, v_rel_BP, v_rel_PROFILE)
-#    TR = clip(TR * TRatio, 0., 1.9)
-                
+    baseTR = interp(float(cruise_gap), [1., 2., 3.], [0.9, 1.3, 1.8])
+    
+    if v_ego <= 20.0:
+      TR = interp(-v_rel, [-0.1, 2.5], [baseTR, 2.1])
+    else:
+      TR = interp(-v_rel, [0, 3.5], [baseTR, 1.8])
+      
     if self.cruise_gap != cruise_gap:
       self.cruise_gap = cruise_gap
       self.libmpc.init(MPC_COST_LONG.TTC, MPC_COST_LONG.DISTANCE, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
