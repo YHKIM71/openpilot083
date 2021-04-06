5b58cfb819e43c185a8cdb1ed7cfd249f9314992
linu1983 <linu1983@gmail.com>
2021-04-07 06:40:29 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -393,7 +393,7 @@ class Controls:
     params = self.sm['liveParameters']
     x = max(params.stiffnessFactor, 0.1)
 #    sr = max(params.steerRatio, 0.1)
-    sr = CP.steerRatio
+    sr = self.CP.steerRatio
     self.VM.update_params(x, sr)
 
     lat_plan = self.sm['lateralPlan']
