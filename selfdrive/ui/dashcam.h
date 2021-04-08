bfabbdd463c3e515effab7bde97a1d436cd1848c
linu1983 <linu1983@gmail.com>
2021-04-08 18:14:57 +0900

    Update dashcam.h

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -319,7 +319,11 @@ void screen_toggle_lock() {
   }
 }
 
-void dashcam( UIState *s, int touch_x, int touch_y ) {
+
+bool dashcam( UIState *s, int touch_x, int touch_y ) {
+
+  bool touched = false;
+  
   screen_draw_button(s, touch_x, touch_y);
   if (screen_button_clicked(touch_x,touch_y)) {
     click_elapsed_time = get_time() - click_time;
@@ -327,15 +331,23 @@ void dashcam( UIState *s, int touch_x, int touch_y ) {
     if (click_elapsed_time > 0) {
       click_time = get_time() + 1;
       screen_toggle_record_state();
+      touched = true;
     }
   }
 
   if (screen_lock_button_clicked(touch_x,touch_y,lock_button)) {
     screen_toggle_lock();
+    touched = true;
   }
   if (!s->vipc_client->connected) {
     // Assume car is not in drive so stop recording
     stop_capture();
   }
-  s->scene.recording = (captureState != CAPTURE_STATE_NOT_CAPTURING);
+
+  if (s->scene.car_state.getVEgo() < 1.5 && !s->scene.controls_state.getEnabled()) {
+    stop_capture();
+  }
+  //s->scene.recording = (captureState != CAPTURE_STATE_NOT_CAPTURING);
+  
+  return touched;
 }
